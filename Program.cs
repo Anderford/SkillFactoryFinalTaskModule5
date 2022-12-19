efa0aaf23a33d0fd7eba614d1f9d5a84507a7019
Anderford <incoqwert@gmail.com>
2022-12-19 22:19:11 +0300

    Final Task start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        GetValues();
+
+        static void GetValues()
+        {
+
+            (string Name, 
+             string Firstname, 
+             int Age,
+             bool Pets
+             ) Person;
+
+
+            Console.WriteLine("Ваше имя");
+            Person.Name = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия");
+            Person.Firstname = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Ваш возраст");
+                Person.Age = Convert.ToInt32(Console.ReadLine());
+                if (!InputExam(Person.Age))
+                    Console.WriteLine("Ваш возраст (цифрами)");
+            } while (!InputExam(Person.Age));
+
+
+            Console.WriteLine($"{Person.Name} {Person.Firstname} {Person.Age}");
+        }
+
+
+
+
+
+
+        static bool InputExam(int number)
+        {
+            return (number > 0 && number < 100);
+        }
+        
+
+    }
+}
+
+
