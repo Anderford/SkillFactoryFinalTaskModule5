ddde8226e73ec221e21edba0c133247c37f433a3
Anderford <incoqwert@gmail.com>
2022-12-20 23:22:31 +0300

    Final Task

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Drawing;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
 
-        GetValues();
+        ShowPerson(GetValues());
 
-        static void GetValues()
+        static (string Name, string Firstname, int Age, bool HavePets, int NumberOfPets, string[] PetName, int NumOfFavColor, string[] FavColors) GetValues()
         {
 
-            (string Name, 
-             string Firstname, 
-             int Age,
-             bool Pets
-             ) Person;
+            (string Name, string Firstname, int Age, bool HavePets, int NumberOfPets, string[] PetName, int NumOfFavColor, string[] FavColors) Person;
+
+            Person.HavePets = false;
+            Person.PetName = new string[0];
+            Person.FavColors = new string[0];
 
 
             Console.WriteLine("Ваше имя");
@@ -24,27 +25,101 @@ class MainClass
 
             do
             {
-                Console.Write("Ваш возраст");
+                Console.WriteLine("Ваш возраст");
                 Person.Age = Convert.ToInt32(Console.ReadLine());
-                if (!InputExam(Person.Age))
+                if (Person.Age < 0 || Person.Age > 100)
                     Console.WriteLine("Ваш возраст (цифрами)");
-            } while (!InputExam(Person.Age));
+            } while (Person.Age < 0 || Person.Age > 100);
 
+            Console.WriteLine("Есть ли у вас питомец?");
+            var Pets = (Console.ReadLine());
+            
+                if(Pets == "да")
+                {
+                    Person.HavePets = true;
+                }
 
-            Console.WriteLine($"{Person.Name} {Person.Firstname} {Person.Age}");
+                else if(Pets == "нет")
+                {
+                    Person.HavePets = false;
+                }
+
+            if (Person.HavePets == true)
+            {
+                Console.WriteLine("Сколько у вас питомцев?");
+                Person.NumberOfPets = Convert.ToInt32(Console.ReadLine());
+                if(Person.NumberOfPets > 0 || Person.NumberOfPets < 100)
+                {
+                    Person.PetName = PersonPets(Person.NumberOfPets);
+                }
+            }
+
+            else
+            {
+                Person.PetName = new string[0];
+                Person.NumberOfPets = 0;
+            }
+
+            Console.WriteLine("Сколько у вас любимых цветов?");
+            Person.NumOfFavColor = Convert.ToInt32(Console.ReadLine());
+            if (Person.NumOfFavColor > 0 || Person.NumOfFavColor < 100)
+            {
+                Person.FavColors = PersonColor(Person.NumOfFavColor);
+            }
+
+            else
+            {
+                Person.FavColors = new string[0];
+            }
+            return Person;
         }
 
-
-
-
-
-
-        static bool InputExam(int number)
+        static string[] PersonPets(int NumberOfPets)
         {
-            return (number > 0 && number < 100);
+
+            var PetsName = new string[NumberOfPets];
+
+            for (int i = 0; i < NumberOfPets; i++)
+            {
+               
+                Console.WriteLine($"Кличка {i + 1} питомца");
+                PetsName[i] = Console.ReadLine();
+
+                
+            }
+            return PetsName;
+        }
+
+        static string[] PersonColor(int NumOfFavColor)
+        {
+            var FavColors = new string[NumOfFavColor];
+
+            for (int i = 0; i < NumOfFavColor; i++)
+            {
+                Console.WriteLine($"{i + 1}-й цвет");
+                FavColors[i] = Console.ReadLine();
+            }
+            return FavColors;
         }
         
+        static  void ShowPerson((string Name, string Firstname, int Age, bool HavePets, int NumberOfPets, string[] PetName, int NumOfFavColor, string[] FavColors) Person)
+        {
+            Console.WriteLine($"Вас зовут {Person.Name} {Person.Firstname} \nВам {Person.Age}");
 
+            Console.Write("Ваши питомцы: ");
+            foreach (var pet in Person.PetName)
+            {
+                Console.Write(pet + ", ");            
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Ваши любимые цвета: ");
+            foreach (var color in Person.FavColors)
+            {
+                Console.Write(color + ", ");
+            }
+        }
     }
 }
 
